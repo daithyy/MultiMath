ed40bd423961e3b21d38d3ab714f4a46aeb2b9ab
David Brennan <davidwbrennan@hotmail.com>
2020-06-02 16:58:17 +0100

    Add type annotation and default parameters.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,25 @@
 function startGame() {
-    var playerName = 'Audrey';
+    var playerName = getInputValue('playername');
     logPlayer(playerName);
-    var messagesElement = document.getElementById('messages');
-    messagesElement.innerText = 'Welcome to MultiMath! Starting new game...';
+    postScore(100, playerName);
 }
 function logPlayer(name) {
+    if (name === void 0) { name = 'MultiMath Player'; }
     console.log("New game starting for player: " + name);
+}
+function getInputValue(elementID) {
+    var inputElement = document.getElementById(elementID);
+    if (inputElement.value === '') {
+        return undefined;
+    }
+    else {
+        return inputElement.value;
+    }
+}
+function postScore(score, playerName) {
+    if (playerName === void 0) { playerName = 'MultiMath Player'; }
+    var scoreElement = document.getElementById('postedScores');
+    scoreElement.innerText = score + " - " + playerName;
 }
 document.getElementById('startGame').addEventListener('click', startGame);
 //# sourceMappingURL=app.js.map

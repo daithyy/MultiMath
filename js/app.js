f82e816b36ce42cbbb6a7292d7e4110871d2367b
David Brennan <davidwbrennan@hotmail.com>
2020-06-02 15:57:49 +0100

    Configure webpack.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 "use strict";
 function startGame() {
-    // Starting a new game
     var messagesElement = document.getElementById('messages');
     messagesElement.innerText = 'Welcome to MultiMath! Starting new game...';
 }
